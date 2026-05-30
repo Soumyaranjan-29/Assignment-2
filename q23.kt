b3ea47d71cd53e9fe2dcf43197a237182814c4b1
Soumyaranjan-29 <jena38949@gmail.com>
2026-05-30 17:40:40 +0530

    Create q23.kt

---
+++ q23.kt
@@ -0,0 +1,21 @@
+//The Concurrent Downloader
+ import kotlinx.coroutines.*
+suspend fun fetchUserData(): String {
+    delay(1000)
+    return "Alice"
+}
+suspend fun fetchAppConfig(): String {
+    delay(2000)
+    return "Dark Mode"
+}
+fun main() = runBlocking {
+
+    val userData = async {
+        fetchUserData()
+    }
+    val appConfig = async {
+        fetchAppConfig()
+    }
+    println("User: ${userData.await()}")
+    println("Config: ${appConfig.await()}")
+}

fc3523c986209893efb12df2c42f1d167416cd69
Soumyaranjan-29 <jena38949@gmail.com>
2026-05-30 16:12:01 +0530

    Create q10.kt

---
+++ q10.kt
@@ -0,0 +1,10 @@
+//Sealed Class
+ sealed class ScreenState {
+    object Loading : ScreenState()
+    data class Success(val data: String): ScreenState()
+}
+
+fun main() {
+    val state: ScreenState = ScreenState.Success("Data Loaded")
+    println(state)
+}

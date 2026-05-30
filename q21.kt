edc73355f63712311a80c7b177cda6c7aaae710f
Soumyaranjan-29 <jena38949@gmail.com>
2026-05-30 17:23:49 +0530

    Create q21.kt

---
+++ q21.kt
@@ -0,0 +1,27 @@
+//The Map/Filter
+ data class Product(
+    val name: String,
+    val price: Double
+)
+
+fun main() {
+
+    val inventory = listOf(
+        Product("Laptop", 1000.0),
+        Product("Mouse", 25.0),
+        Product("Keyboard", 60.0)
+    )
+
+    val discountedProducts = inventory
+        .map {
+            Product(it.name, it.price * 0.9)
+        }
+       .filter {
+            it.price > 50
+       }
+
+    println("Products after discount and filtering:")
+    discountedProducts.forEach {
+        println("${it.name} : $${it.price}")
+    }
+}
